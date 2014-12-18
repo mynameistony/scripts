49dfa0fb5717db2e6b8d333d8a0f623558bc695c
Tony <tonyrogers10195@gmail.com>
2014-12-17 22:10:26 -0800

    Shell script to download reddit links

--- reddit.sh
+++ reddit.sh
@@ -1,9 +1,80 @@
 #!/bin/bash
 
-sub=`matedialog --entry`
+photoDir="/home/$USER/Pictures/Reddit"
 
-curl -s www.reddit.com/r/$sub.json | grep -o "http://i.imgur.com/[A-Za-z0-9]*.jpg" > /home/tony/Pictures/Reddit/get.txt
+if [ $# -eq 1 ]
+then
+	sub=$1
+else
+	sub=`matedialog --entry`
+fi
 
-cd /home/tony/Pictures/Reddit
 
-wget -N -q -i /home/tony/Pictures/Reddit/get.txt
+test -e $photoDir
+
+if [ $? -ne 0 ]
+then
+	mkdir "$photoDir"
+fi
+
+
+test -e "$photoDir/$sub"
+
+if [ $? -ne 0 ]
+then
+	mkdir $photoDir/$sub
+fi
+
+titles="$photoDir/$sub/titles.txt"
+links="$photoDir/$sub/links.txt"
+
+echo -n "Getting titles..."
+curl -s --no-keepalive -A "Mozilla" "http://www.reddit.com/r/$sub.json" | grep "\"title\": \"[-A-Za-z0-9 \"#/\!?()_,'.\[*\]*]*" -o > "$titles"
+echo "Done[$?]"
+
+echo -n "Getting links..."
+curl -s --no-keepalive -A "Mozilla" "http://www.reddit.com/r/$sub.json" | grep "\"url\": \"[-A-Za-z0-9 %:\"#/\!?()_'.\[*\]*]*" -o > "$links"
+echo "Done[$?]"
+
+echo -n "Titles: "
+cat $titles | wc -l
+
+echo -n "Links: "
+cat $links | wc -l
+
+sed s/"\"title\": "/""/ "$titles" | sed s/", \"created_utc\""/""/ | sed s/"\s"/""/ | sed s/"\s"/""/ | sed s/"\s"/""/ > $titles.tmp
+
+cat "$titles.tmp" > "$titles"
+
+grep "http://[A-Za-z0-9./_]*" $links -o > $links.tmp
+
+cat $links.tmp > $links
+
+rm $photoDir/$sub/*.tmp
+
+cd $photoDir/$sub
+
+echo -n "Downloading links..."
+wget -q -N -i "$links"
+echo "Done"
+
+echo "View Links? "
+read prompt
+
+if [ $prompt == "y" ]
+then
+	eom *
+fi
+
+#for title in `cat "$titles"`
+#do
+#	for link in `cat "$links"`
+#	do
+#		echo "Title: "
+#		echo "$title"
+#		echo "Link: "
+#		echo "$link"
+#	done
+#done
+
+exit 0
