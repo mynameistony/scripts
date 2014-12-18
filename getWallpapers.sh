b9120a6341852a524293fb6e18c4d32ac7507031
Tony <tonyrogers10195@gmail.com>
2014-12-17 22:16:03 -0800

    Updated

--- getWallpapers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -s www.reddit.com/r/wallpapers.json | grep -o "http://i.imgur.com/[A-Za-z0-9]*.jpg" > /home/tony/Pictures/Wallpapers/wallpapers.txt
-
-cd /home/tony/Pictures/Wallpapers
-
-wget -N -q -i /home/tony/Pictures/Wallpapers/wallpapers.txt
--- til.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -s http://www.reddit.com/r/todayilearned/|perl -ne 'while (m/<a class="title[^>]*>([^<]*)/g) { print "$1\n"};' >> facts.txt
-
-facts=`cat facts.txt`
-
-notify-send "$facts"
